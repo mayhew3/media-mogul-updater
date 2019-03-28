baa2b8acf080b1a00728b63babc350ea361c1a44
mayhew3 <commander.boffo@gmail.com>
2019-03-28 12:32:57 -0700

    MM-709: Add simple task in new module

---
+++ scheduler/src/main/java/com/heroku/devcenter/SchedulerSimpleTask.java
@@ -0,0 +1,11 @@
+package com.heroku.devcenter;
+
+/**
+ * Simple test task
+ */
+public class SchedulerSimpleTask {
+
+    public static void main(String[] args) {
+        System.out.println("Scheduler Task complete M3!");
+    }
+}
