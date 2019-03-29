8ccf54c31011e39d5ad006f7cb3d9b0403ce297e
mayhew3 <commander.boffo@gmail.com>
2019-03-28 18:23:03 -0700

    MM-709: Try importing my library

---
+++ common/src/main/java/com/heroku/devcenter/Doug.java
@@ -0,0 +1,10 @@
+package com.heroku.devcenter;
+
+import com.mayhew3.postgresobject.dataobject.*;
+
+public class Doug extends RetireableDataObject {
+  @Override
+  public String getTableName() {
+    return null;
+  }
+}
